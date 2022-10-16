46f341c091c1fe34dc61770c767b7282b5b39705
beomshin.kim <chzh859@naver.com>
2022-10-16 19:51:55 +0900

    # Q33 ~ Q36

---
+++ src/Q36.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q36 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[N+1];
+
+
+        arr[1] = arr[2] = arr[3] = "*";
+        int num = 1;
+
+        while (num < N) {
+
+            for (int i=1; i <= num; i++) {
+                String star = arr[i];
+                arr[i] = star.concat(star).concat(star);
+                arr[i + num] = star.concat(star.replace("*", " ")).concat(star);
+                arr[i + (num * 2)] = star.concat(star).concat(star);
+            }
+            num *= 3;
+        }
+
+        for (int i=1; i <= N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
