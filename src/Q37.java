c936d7ecb045e8ac401a1a805a0236df55db90a5
beomshin.kim <chzh859@naver.com>
2022-10-17 00:02:25 +0900

    # Q37 ~ Q38

---
+++ src/Q37.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q37 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i < T; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int[] coin = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j < N; j++) {
+                coin[j] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+
+            calculate(coin, M, N);
+        }
+
+
+    }
+
+    public static void calculate(int[] coin, int target, int N) {
+
+        long[] dp = new long[target+1];
+
+       dp[0] = 1;
+
+        for (int i=0 ; i < N; i++) {
+            int cost = coin[i];
+
+            for (int j=0; j <= target; j++) {
+                if (j >= cost) {
+                    dp[j] += dp[j - cost];
+                }
+            }
+
+        }
+
+        System.out.println(dp[target]);
+    }
+}
---
+++ src/Q38.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Q38 {
+
+    static int N, K;
+    static int[][] info;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        info = new int[N][2];
+
+        for (int i=0 ; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            info[i][0] = w;
+            info[i][1] = v;
+        }
+
+        int[][] dp = new int[N+1][K+1];
+
+        for (int i=1; i <= K; i++) {
+
+            for (int j=1; j <= N; j++) {
+
+                dp[j][i] = dp[j-1][i];
+                int weight = info[j-1][0];
+                int value = info[j-1][1];
+
+                if (i - weight >= 0) {
+                    dp[j][i] = Math.max(dp[j-1][i], dp[j-1][i - weight] + value);
+                }
+
+
+            }
+
+        }
+
+        for (int i=0 ; i < dp.length; i ++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
